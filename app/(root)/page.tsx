3dfe4761c53104477e95cdea996abbffe8bb1ed2
markdauz <87331275+markdauz@users.noreply.github.com>
2025-05-29 13:16:55 +0800

    serverless env config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import { getLatestProducts } from "@/lib/actions/product.actions";
 import ProductList from "@/components/shared/product/product-list";
 const Home = async () => {
   const latestProducts = await getLatestProducts();
-  return (<>
-    <ProductList data={latestProducts} title='Featured Products' limit={4} />
-  </>);
+  return (
+    <>
+      <ProductList data={latestProducts} title='Featured Products' limit={4} />
+    </>);
 }
 
 export default Home;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,10 +1,10 @@
 'use server';
-import { PrismaClient } from '@/lib/generated/prisma/client';
+
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { prisma } from '@/db/prisma';
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
