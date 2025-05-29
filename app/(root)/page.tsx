fe86ab8340406ee265bc804176c2ea77d94a61b6
markdauz <87331275+markdauz@users.noreply.github.com>
2025-05-29 09:27:14 +0800

    added sample data, product component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const Home = () => {
-  return (<>Ecommerce...</>);
+  return (<>
+    <ProductList data={sampleData.products} title='Featured Products' limit={4} />
+  </>);
 }
 
 export default Home;
