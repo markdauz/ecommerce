a6143899cd47433f37407c16790857de41e6bbb8
markdauz <87331275+markdauz@users.noreply.github.com>
2025-05-30 09:17:31 +0800

    added sign in form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,7 @@ export const SERVER_URL =
 export const LATEST_PRODUCTS_LIMIT = Number(
   process.env.NEXT_PUBLIC_LATEST_PRODUCTS_LIMIT || 4
 );
+export const signInDefaultValues = {
+  email: 'admin@example.com',
+  password: '123456',
+};
