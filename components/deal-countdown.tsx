753a630ee219cbf80f8862a9664efc094d03039e
markdauz <87331275+markdauz@users.noreply.github.com>
2025-09-11 14:58:39 +0800

    cart persist on signout fix

--- components/deal-countdown.tsx
+++ components/deal-countdown.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 // Static target date (replace with desired date)
-const TARGET_DATE = new Date('2025-01-20T00:00:00');
+const TARGET_DATE = new Date('2025-11-20T00:00:00');
 
 // Function to calculate the time remaining
 const calculateTimeRemaining = (targetDate: Date) => {
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -17,6 +17,7 @@ import { z } from 'zod'
 import { PAGE_SIZE } from '../constants'
 import { Prisma } from '@/lib/generated/prisma/client'
 import { revalidatePath } from 'next/cache'
+import { getMyCart } from './cart.actions'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -42,6 +43,9 @@ export async function signInWithCredentials(
 
 // Sign user out
 export async function signOutUser() {
+  await signOut()
+  const currentCart = await getMyCart()
+  await prisma.cart.delete({ where: { id: currentCart?.id } })
   await signOut()
 }
 
